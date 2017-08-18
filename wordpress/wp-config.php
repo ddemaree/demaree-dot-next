99e0c1e24c750178b2da61f60f6fc5cec3ea4460
David Demaree <david@demaree.me>
2017-08-18 17:52:08 -0400

    Add lines to wp-config to disable auto updates

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -19,33 +19,14 @@
  */
 
 // ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
 define('DB_NAME', 'wordpress');
-
-/** MySQL database username */
 define('DB_USER', $_ENV['WORDPRESS_DB_USER']);
-
-/** MySQL database password */
 define('DB_PASSWORD',  $_ENV['WORDPRESS_DB_PASSWORD']);
-
-/** MySQL hostname */
 define('DB_HOST', 'mysql');
-
-/** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
+// ** Authentication Unique Keys and Salts. */
 define('AUTH_KEY',         'bdac1997b75a37efc43bd366441bd999cb0880aa');
 define('SECURE_AUTH_KEY',  '602529d16ffa7b5e2b2aaadd40dfb49e606ed2e2');
 define('LOGGED_IN_KEY',    '25e1e04570137c1b630e4e33189e0e15cb95b43b');
@@ -55,14 +36,6 @@ define('SECURE_AUTH_SALT', '09c146b61e30c0ad5e214ca735bcffa0e7fe7bf4');
 define('LOGGED_IN_SALT',   '8abcddbb1b8439fbd9bf16ad63145a41af5a9f3a');
 define('NONCE_SALT',       'f761f9c0f59f15ce5a62c861c2e353f8554f5062');
 
-/**#@-*/
-
-/**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
- */
 $table_prefix  = 'wp_';
 
 /**
@@ -79,8 +52,14 @@ $table_prefix  = 'wp_';
  */
 define('WP_DEBUG', false);
 
-// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
-// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+define( 'WP_ALLOW_MULTISITE', true );
+
+// Disable automatic updates
+define( 'DISALLOW_FILE_EDIT', true );
+define( 'DISALLOW_FILE_MODS', true );
+define( 'AUTOMATIC_UPDATER_DISABLED', true );
+define( 'WP_AUTO_UPDATE_CORE', false );
+
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
 	$_SERVER['HTTPS'] = 'on';
 }

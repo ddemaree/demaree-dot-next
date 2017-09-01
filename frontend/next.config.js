db2d544f71e23fad383df9a26cf3597274f05d98
David Demaree <david@demaree.me>
2017-09-01 18:58:11 -0400

    Add separate env var for WP API on client side

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,8 +3,16 @@ const webpack = require('webpack');
 module.exports = {
   webpack: (config, { dev }) => {
     // env vars that should be visible to the client must be whitelisted here
+    // When running in Docker with local WordPress, need to provide a separate env var for the client-side API since `wordpress` isn't visible outside the container
+    let POSTS_API;
+    if(process.env.POSTS_CLIENT_API) {
+      POSTS_API = JSON.stringify(process.env.POSTS_CLIENT_API);
+    } else {
+      POSTS_API = JSON.stringify(process.env.POSTS_API);
+    }
+
     const defines = new webpack.DefinePlugin({
-      'process.env.POSTS_API': JSON.stringify(process.env.POSTS_API)
+      'process.env.POSTS_API': POSTS_API
     });
     config.plugins.push(defines);
 

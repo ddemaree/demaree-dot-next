16d5c7291ee8c69a0856ad8d1aca6f29e22e704d
David Demaree <david@demaree.me>
2017-09-01 18:59:19 -0400

    Move homepage content into Home component with React fanciness

---
+++ frontend/components/home.js
@@ -0,0 +1,97 @@
+import React from 'react'
+
+export default class Home extends React.Component {
+
+  componentDidMount() {
+    const { body } = document;
+    body.classList.add('body-home');
+
+    // Only show/load image after initial page load when React mounts
+    // this.elem.classList.add('show-image');
+    setTimeout(()=>{
+      this.img.classList.add('show');
+      setTimeout(() => {
+        this.text.classList.add('show');
+      }, 2000);
+    }, 1000);
+
+    this.setElementHeight();
+    window.addEventListener('resize', this.setElementHeight.bind(this));
+  }
+
+  componentWillUnmount() {
+    document.body.classList.remove('body-home');
+    window.removeEventListener('resize', this.setElementHeight.bind(this));
+  }
+
+  setElementHeight(event) {
+    const [ header ] = document.getElementsByClassName('site-header');
+    let newHeight;
+
+    if(window.innerWidth <= 700){
+      newHeight = Math.ceil(window.innerHeight - header.offsetHeight);
+    } else {
+      newHeight = window.innerHeight;
+    }
+    this.elem.style.minHeight = `${newHeight}px`;
+  }
+
+  render() {
+    return (
+      <div className="home" ref={(elem) => { this.elem = elem; } }>
+        <div className="home-img" ref={(el) => { this.img = el; }} />
+        <div className="home-text" ref={(el) => { this.text = el; }}>
+          <p>Designers like to put big text on their home pages. Perhaps a quotation. Sometimes a slogan. WTF is up with that?</p>
+        </div>
+
+        <style jsx>{`
+        .home {
+          background-color: #f0f;
+          background-image: linear-gradient(0deg, #f0f, #ffcf00);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          position: relative;
+        }
+        .home-img {
+          background-image: url('/static/images/chicago.jpg');
+          background-size: cover;
+          background-position: center 30%;
+          position: absolute;
+          top: 0;
+          left: 0;
+          bottom: 0;
+          width: 100%;
+          z-index: 1;
+          opacity: 0;
+          transition: opacity 5000ms;
+        }
+        .home-img.show {
+          opacity: 0.4;
+        }
+        .home-text {
+          position: relative;
+          z-index: 10;
+          font-weight: 600;
+          text-transform: uppercase;
+          padding: 1em;
+          line-height: 1.75;
+          letter-spacing: 0.1em;
+          font-size: 2.0em;
+          color: #fff;
+          text-shadow: 0 0 60px rgba(0,0,0,0.4);
+          text-align: center;
+          opacity: 0;
+        }
+        .home-text.show {
+          opacity: 1;
+          transition: opacity 5000ms;
+        }
+        .home-text p {
+          margin: 0;
+        }
+        `}</style>
+      </div>
+    )
+  }
+}
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,47 +1,9 @@
 import React from 'react'
 import PageLayout from 'components/page-layout'
+import Home from 'components/home'
 
-export default class Home extends React.Component {
-
-  render() {
-    return (
-      <PageLayout section="home">
-        <div className="home">
-          <div className="home-image" />
-          <div className="home-text">
-            <p>Designers like to put big text on their home pages. Perhaps a quotation. Sometimes a slogan.</p>
-          </div>
-        </div>
-        <style jsx global>{`
-        .site-header {
-          /* background-color: #f0f; */
-        }
-        .container {
-          background: #000;
-        }
-        `}</style>
-        <style jsx>{`
-        .home {
-          background-image: url('/static/images/chicago.jpg');
-          background-size: cover;
-          background-position: center 30%;
-          min-height: 100vh;
-        }
-        .home-text {
-          {/* position: fixed;
-          left: 240px;
-          top: 0;
-          bottom: 0;
-          width: calc(100% - 240px); */}
-          font-size: 2em;
-          color: #fff;
-          text-shadow: 0 0 10px #000;
-        }
-        .home-text p {
-        }
-        `}</style>
-      </PageLayout>
-    );
-  }
-
-}
+export default () => (
+  <PageLayout section="home">
+    <Home />
+  </PageLayout>
+)

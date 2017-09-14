a4654e4cc9d18e6da80d9568c1d2f4853ad04fa7
David Demaree <david@demaree.me>
2017-09-14 09:39:56 -0400

    Move frontend code into the root
    Goodbye WordPress

--- wordpress/themes/birthdaysuit/content.php
+++
@@ -1,8 +0,0 @@
-<article class="post post--standard">
-  <header class="post__header">
-    <h2><? the_title(); ?></h2>
-  </header>
-  <div class="post__content">
-    <? the_content(); ?>
-  </div>
-</article>
--- wordpress/themes/birthdaysuit/footer.php
+++
@@ -1,8 +0,0 @@
-</main>
-</div> <!-- /.container -->
-
-<?php wp_footer(); ?>
-<script src="//<?php echo $_ENV['ASSET_HOST'] ?>/assets/main.js"></script>
-
-</body>
-</html>
